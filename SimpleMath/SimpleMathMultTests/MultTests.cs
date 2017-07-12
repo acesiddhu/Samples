e430b8e5af5aec153e0f7185dd8b86c25f0eee8a
Prawal Agarwal <praga@microsoft.com>
2017-07-12 14:36:49 +0530

    Update MultTests.cs

--- SimpleMath/SimpleMathMultTests/MultTests.cs
+++ SimpleMath/SimpleMathMultTests/MultTests.cs
@@ -16,7 +16,7 @@ namespace SimpleMath.MultTests
         {
             var i = new SimpleMath.Mult().mult(5, 4);
             Assert.AreEqual<int>(20, i);
-            //Commentasdfsadfasdf
+            //Commentasdfsadfasdffasdf
         }
     }
 }
